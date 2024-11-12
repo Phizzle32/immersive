3658f9d915800fc6eaf0ccff2796f0d5cf4be36f
Phi Le <phizzle2003@gmail.com>
2024-11-11 23:20:41 -0500

    Added ability to run frontend and backend concurrently

--- backend/database.js
+++ backend/database.js
@@ -9,3 +9,13 @@ export const db = mysql.createPool({
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
 }).promise();
+
+export const checkDatabaseConnection = async () => {
+    try {
+        const connection = await db.getConnection();
+        console.log('Database connection established');
+        connection.release();
+    } catch (err) {
+        throw new Error(err);
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -4,31 +4,32 @@ import itemRoutes from './routes/item.js';
 import reviewRoutes from './routes/review.js';
 import userRoutes from './routes/user.js';
 import transactionRoutes from './routes/transaction.js';
-import { db } from "./database.js";
+import { db, checkDatabaseConnection } from "./database.js";
 
 const app = express();
 const PORT = 8080;
 
-app.get('/', (req, res) => {
-    res.send('Server is running');
-});
+checkDatabaseConnection().then(() => {
+    app.get('/', (req, res) => {
+        res.send('Server is running');
+    });
 
-app.use('/category', categoryRoutes);
-app.use('/item', itemRoutes);
-app.use('/review', reviewRoutes);
-app.use('/user', userRoutes);
-app.use('/transaction', transactionRoutes);
+    // API routes
+    app.use('/category', categoryRoutes);
+    app.use('/item', itemRoutes);
+    app.use('/review', reviewRoutes);
+    app.use('/user', userRoutes);
+    app.use('/transaction', transactionRoutes);
 
-process.on('unhandledRejection', err => {
-    console.log(err);
+    process.on('SIGINT', () => {
+        console.log('Stopping server...');
+        db.end();
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}).catch((error) => {
+    console.log("Could not connect to database:", error);
     process.exit(1);
-});
-
-process.on('SIGINT', () => {
-    console.log('Stopping server...');
-    db.end();
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+} );
